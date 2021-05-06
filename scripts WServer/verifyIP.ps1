e7fa2e976207f002e817cff540e2cdf7d27285bd
José Carlos Menéndez <73886386+Jmenendezasir@users.noreply.github.com>
2021-05-06 10:19:50 +0200

    Verify IP v2

--- scripts WServer/verifyIP.ps1
+++ scripts WServer/verifyIP.ps1
@@ -1,17 +1,17 @@
-﻿   clear
-      $Cadena = Read-Host "Dame Dirección IP (xxx.xxx.xxx.xxx): "
-   if ($Cadena -match "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$" )
-      {   
-        if ([ipaddress]::TryParse(($Cadena),[ref][ipaddress]::Loopback))  
-            {
+﻿clear
+    $Cadena = Read-Host "Dame dirección IP (xxx.xxx.xxx.xxx): "
+if ($Cadena -match "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$" )
+    {
+        if([ipaddress]::TryParse(($Cadena),[ref][ipaddress]::Loopback))
+        {
             echo "La dirección IP es correcta"
-            }
-      else
-          {
-             echo "La dirección IP es incorrecta"
-          } 
-       }
-     else 
-       { 
-        echo "La cadena de la IP $CADENA no es correcta"
-       }
+        }
+ else
+        {
+            echo "La dirección IP es incorrecta."
+        }
+    }
+   else
+   {
+    echo "La cadena de la IP $Cadena no es correcta."
+   }
